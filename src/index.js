ce7a065ace849db8be2df6ca2c3c41b1887faf02
Pavel Fedorenko <winterspray@gmail.com>
2023-01-10 19:55:57 +0200

    dd

--- src/index.js
+++ src/index.js
@@ -36,7 +36,7 @@ export const values = (object) => {
 export const entries = (object) => {
   let arr = [];
   for (let key in object) {
-    arr.push(`${key}${object[key]}`);
+    arr.push(key).push(object[key]);
   }
   return arr;
   //   return Object.entries(object);
